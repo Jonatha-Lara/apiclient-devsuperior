2929018028b255cd79aa275629f24ede78ae4d72
Jonatha Lara <jonathalarasilverio@gmail.com>
2023-01-20 22:52:05 -0300

    Project teste1 DevSuperior

---
+++ backend/src/test/java/com/apiclient/apiClient/ApiClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apiclient.apiClient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
